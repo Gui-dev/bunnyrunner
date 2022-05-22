c125b3b39e0b69c546fc1420cdb06abba2ede570
Gui-dev <adrianon10@gmail.com>
2022-05-22 19:30:54 -0300

    feat: add speed and jump adjustment

---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+const GRAVITY = 1500
+var speed_direction_x = 500
+var velocity = Vector2(0, 0)
+var jump = false
+
+func _ready() -> void:
+  set_process_input(true)
+
+func _physics_process(delta: float) -> void:
+  velocity.y += GRAVITY * delta
+  velocity.x = speed_direction_x
+  velocity = move_and_slide(velocity, Vector2(0, -1))
+  
+  if is_on_floor():
+    if jump:
+      velocity.y = -800
+  
+  jump = false
+
+
+func _input(event):
+  if event is InputEventScreenTouch:
+    if event.pressed:
+      jump = true
