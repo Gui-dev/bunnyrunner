f289736ec4f62b6355ac24bd95d31a05cf0b8cd5
Gui-dev <adrianon10@gmail.com>
2022-05-23 02:27:55 -0300

    feat: add control jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ const GRAVITY = 1500
 var speed_direction_x = 500
 var velocity = Vector2(0, 0)
 var jump = false
+var jump_release = false
 
 func _ready() -> void:
   set_process_input(true)
@@ -16,13 +17,18 @@ func _physics_process(delta: float) -> void:
   if is_on_floor():
     $sprite.play('walk')
     if jump:
-      velocity.y = -800
+      velocity.y = -1000
   else:
     $sprite.play('jump')
+    if jump_release:
+      velocity.y *= 0.3
   
   jump = false
+  jump_release = false
 
 func _input(event):
   if event is InputEventScreenTouch:
     if event.pressed:
       jump = true
+    else:
+      jump_release = true
