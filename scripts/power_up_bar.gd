ddd816511765ff411dddb97c0ffd21e3e418fa19
Gui-dev <adrianon10@gmail.com>
2022-06-08 02:27:48 -0300

    feat: add helper function count, stop and _on_tween_tween_completed

--- scripts/power_up_bar.gd
+++ scripts/power_up_bar.gd
@@ -8,4 +8,16 @@ func _ready() -> void:
 
 func start(time: float) -> void:
   visible = true
-  print('TIME: ', time)
+  $tween.interpolate_method(self, 'count', 1, 0, time, Tween.EASE_IN, 0)
+  $tween.start()
+  
+func count(value: float) -> void:
+  $bar.rect_size = Vector2(size.x * value, size.y)
+
+func stop() -> void:
+  $tween.stop_all()
+  visible = false
+
+func _on_tween_tween_completed(object: Object, key: NodePath) -> void:
+  stop()
+  get_tree().call_group('player', 'power_up_finished')
