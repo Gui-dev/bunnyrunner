fa83405d475bf564a36319c8477865426448f973
Gui-dev <adrianon10@gmail.com>
2022-06-12 19:36:19 -0300

    feat: add loading phases

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
 
+enum {MENU, LOADING, LOADED}
+var status = MENU
+var current_stage
+var loaded_stage
+
+
 func _ready() -> void:
   add_to_group('game')
 
 func stage_selected(button: Button) -> void:
-  print(typeof(button))
-  print(button.stage)
+  if status == MENU:
+    status = LOADING
+    current_stage = button.stage
+    $interface.hide()
+    load_stage()
+    status = LOADED
+
+func load_stage():
+  if loaded_stage != null:
+    loaded_stage.queue_free()
+  loaded_stage = load(current_stage).instance()
+  add_child(loaded_stage)
