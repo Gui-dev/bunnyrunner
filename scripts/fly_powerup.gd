553411df9116c73e7ff1fadcb9669366e080b395
Gui-dev <adrianon10@gmail.com>
2022-06-13 03:13:23 -0300

    feat: add set sprite visible to false

--- scripts/fly_powerup.gd
+++ scripts/fly_powerup.gd
@@ -4,9 +4,10 @@ extends Area2D
 export var time = 10.0 
 
 func _ready() -> void:
-  pass
+  $sprite.visible = true
 
 
 func _on_fly_powerup_body_entered(body: Node) -> void:
   get_tree().call_group('power_up_bar', 'start', time)
   body.fly()
+  $sprite.visible = false
