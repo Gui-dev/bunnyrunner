8995ad8339fd48d2c7d01c2c430b01c1be79798f
Gui-dev <adrianon10@gmail.com>
2022-06-13 03:11:12 -0300

    feat: add counter to start the phase

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,6 +8,7 @@ var loaded_stage
 var reference_stage
 
 func _ready() -> void:
+  $HUD/countdown.visible = false
   add_to_group('game')
 
 func stage_selected(button: Button) -> void:
@@ -25,6 +26,9 @@ func load_stage():
   loaded_stage = load(current_stage).instance()
   reference_stage = weakref(loaded_stage)
   add_child(loaded_stage)
+  $HUD/countdown/animation.play('countdown')
+  yield($HUD/countdown/animation, 'animation_finished')
+  get_tree().call_group('player', 'start')
 
 func player_died():
   load_stage()
